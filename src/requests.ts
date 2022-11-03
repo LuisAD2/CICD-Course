e8b41db86f6ac7a141de3bce8cd289829cc5e459
LuisAD2 <luis.alvaradodgz@gmail.com>
2022-11-03 02:14:23 -0600

    first commit

---
+++ src/requests.ts
@@ -0,0 +1,32 @@
+import { BASE_URL } from "./constants";
+import axios from "axios";
+import { FactDefinition, FactsDefinition } from "./types";
+
+export const isResponseValid = (status: number) => status === 200;
+
+/**
+ * Request cats fact service
+ * @return FactDefinition or undefined
+ * @FactDefinition fact: string; length: number;
+ */
+export const getFact = async (
+  path: string
+): Promise<FactDefinition | undefined> => {
+  const response = await axios.get(`${BASE_URL}${path}`);
+  if (isResponseValid(response.status)) {
+    return response.data as FactDefinition;
+  }
+  return undefined;
+};
+
+/**
+ * Request a list of cat facts
+ * @return FactsDefinition or undefined
+ */
+export const getFactsList = async (): Promise<FactsDefinition | undefined> => {
+  const response = await axios.get(`${BASE_URL}/facts`);
+  if (isResponseValid(response.status)) {
+    return response.data as FactsDefinition;
+  }
+  return undefined;
+};
